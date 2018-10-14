f2f7e7a630034d0d8c30568f9e7644accd5bc7cc
vitaliifokine <vf281992>
2018-10-13 23:32:34 -0400

    7

--- src/tests/GoogleSearchTest.java
+++ src/tests/GoogleSearchTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.ResultPage;
 import pages.SearchPage;
@@ -63,10 +65,47 @@ public class GoogleSearchTest extends BaseTest{
         t.get(0).click();
     }
 
-    @Test
-    public void doGoogleSearch4(){
 
+    @DataProvider(name = "Authentication")
+    public static Object[][] credentials() {
+        return new Object[][] {
+                {"LinkedIn", "LinkedIn: Log In or Sign Up" },
+                {"Google", "Google"},
+                {"Facebook", "Facebook - Log In or Sign Up"},
+                {"Netflix", "Netflix - Watch TV Shows Online, Watch Movies Online"}
+        };
     }
 
+    @Test(dataProvider = "Authentication")
+        public void doGoogleSearch6(String searchItem, String expectedItem){
+            System.setProperty("chromedriver", "/Users/juliakolesnyk/Desktop/LessonSample/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.google.com");
+            driver.findElement(By.cssSelector("div.a4bIc > input")).sendKeys(searchItem);
+            driver.findElement(By.cssSelector("div.a4bIc > input")).sendKeys(Keys.ENTER);
+            WebDriverWait wait = new WebDriverWait(driver, 15);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#logo > img")));
+            List<WebElement> t = driver.findElements(By.cssSelector("#search h3.LC20lb"));
+            for(WebElement i: t) {
+                System.out.println(i.getText()); }
+            Assert.assertEquals(t.get(0).getText(), expectedItem);
+            t.get(0).click();
+        }
 
+    @Test(dataProvider = "Authentication")
+    public void doGoogleSearch7(String volodya, String siteLinkVolodya){
+        System.setProperty("chromedriver", "/Users/juliakolesnyk/Desktop/LessonSample/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.cssSelector("div.a4bIc > input")).sendKeys(volodya);
+        driver.findElement(By.cssSelector("div.a4bIc > input")).sendKeys(Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#logo > img")));
+        List<WebElement> t = driver.findElements(By.cssSelector("#search h3.LC20lb"));
+        for(WebElement i: t) {
+            System.out.println(i.getText());
+        }
+        Assert.assertEquals(t.get(0).getText(), siteLinkVolodya);
+        t.get(0).click();
+    }
 }
